a8110f58a45124764cd7a63a189be7a74a4d6fb7
zxh <2726012413@qq.com>
2018-03-31 16:06:37 +0800

    094

--- 094.py
+++ 094.py
@@ -12,4 +12,33 @@ class Solution(object):
         :rtype: List[int]
         """
         stack = []
-        
+        rList = []
+        currentNode = root
+        while currentNode != None or len(stack)!=0:
+            if currentNode != None:
+                stack.append(currentNode)
+                currentNode = currentNode.left
+
+            else:
+                temp = stack.pop()
+                rList.append(temp.val)
+                currentNode = temp.right
+
+        return rList
+
+if __name__=="__main__":
+    solution = Solution()
+    node1 = TreeNode(1)
+    node2 = TreeNode(2)
+    node3 = TreeNode(3)
+    node4 = TreeNode(4)
+    node5 = TreeNode(5)
+    node6 = TreeNode(6)
+
+    node1.left = node2
+    node1.right = node3
+    node2.left = node4
+    node2.right = node5
+    node3.left = node6
+
+    print(solution.inorderTraversal(node1))
