5e930138023d5b292c4ea0e6dd0e6c720c26c6ac
zxh <2726012413@qq.com>
2018-07-16 23:35:29 +0800

    198

---
+++ 198.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums)==0:
+            return 0
+        elif len(nums)==1:
+            return nums[0]
+        res = [0]*len(nums)
+        res[0] = nums[0]
+        res[1] = max(nums[0], nums[1])
+
+        for i in range(2, len(nums)):
+            res[i] = max(res[i-2]+nums[i], res[i-1])
+        return res[-1]
--- test.py
+++ test.py
@@ -1,2 +1,2 @@
 
-print([[j for j in range(5)] for i in range(10)])
+print()
