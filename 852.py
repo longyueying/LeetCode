3f77c6c47e3293bcdd496903103d767e6170ed84
zxh <2726012413@qq.com>
2018-08-10 19:15:10 +0800

    852

---
+++ 852.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def peakIndexInMountainArray(self, A):
+        """
+        :type A: List[int]
+        :rtype: int
+        """
+        for i in range(len(A)):
+            if A[i] > A[i+1]:
+                break
+        return i
