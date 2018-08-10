bc7ed804c05b16f642f624a6822171ebfd59c7cb
zxh <2726012413@qq.com>
2018-08-10 20:41:59 +0800

    476

---
+++ 476.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def findComplement(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+        i = 1
+        while i < num:
+            i<<1
+        i-=1
+        return i^num
