3ecdec3f24cdac26a435efedd9e0a4219f5a71bb
zxh <2726012413@qq.com>
2018-08-14 19:52:54 +0800

    083

---
+++ 083.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if head == None:
+            return head
+        tmp = head
+        while tmp.next!=None:
+            if tmp.next.val == tmp.val:
+                tmp.next = tmp.next.next
+            else:
+                tmp = tmp.next
+        return head
