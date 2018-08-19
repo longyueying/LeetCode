945f1862e23acf6af94182bf4e85a75027e28a2d
zxh <2726012413@qq.com>
2018-08-19 20:36:35 +0800

    109

---
+++ 109.py
@@ -0,0 +1,30 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def sortedArrayToBST(self, head):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        nums = []
+        while head!=None:
+            nums.append(head.val)
+            head = head.next
+
+        def solver(nums):
+            if len(nums) == 0:
+                return None
+            if len(nums)==1:
+                return TreeNode(nums[0])
+            mid = int(len(nums)/2)
+            t = TreeNode(nums[mid])
+            t.left = solver(nums[:mid])
+            t.right = solver(nums[mid+1:])
+            return t
+        return solver(nums)
+
+
