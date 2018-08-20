53907a577ef53575ff6a149bb558ff76bb5845e4
zxh <2726012413@qq.com>
2018-08-20 20:38:18 +0800

    112

--- 112.py
+++ 112.py
@@ -5,13 +5,30 @@ class TreeNode:
         self.right = None
 
 class Solution:
-    self
+
     def hasPathSum(self, root, sum):
         """
         :type root: TreeNode
         :type sum: int
         :rtype: bool
         """
+        self.res = False
+        self.solver(root,0,sum)
+        return self.res
 
 
-    def solver(self, root):
+
+    def solver(self, root, v, sum):
+        if self.res==True:
+            return
+        if root==None:
+            return
+        v = v + root.val
+        if root.left==None and root.right==None:
+            if v== sum:
+                self.res=True
+            return
+        self.solver(root.left, v, sum)
+        self.solver(root.right, v, sum)
+
+
