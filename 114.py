c90c41d9abe20e458562099297a21f07a0dd5b3a
zxh <2726012413@qq.com>
2018-08-20 21:12:23 +0800

    114

---
+++ 114.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def flatten(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        if root == None:
+            return
+        self.flatten(root.left)
+        self.flatten(root.right)
+
+        if root.left != None:
+            tmp = root.right
+            root.right = root.left
+            root.left=None
+            while root.right != None:
+                root = root.right
+            root.right = tmp
