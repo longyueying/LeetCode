4d6f8f2c578b609fb8b0687310bf6f06d92d640e
zxh <2726012413@qq.com>
2018-08-29 15:18:44 +0800

    unionfind

---
+++ unionfind.py
@@ -0,0 +1,24 @@
+class uninfind:
+    def __init__(self, dimention):
+        self.rootlist = list(range(dimention))
+        self.sizelist = [1]*dimention
+        self.count = dimention
+
+    def find(self, element):
+        if element!=self.rootlist[element]:
+            self.rootlist[element] = self.find(self.rootlist[element])
+        return self.rootlist[element]
+
+    def union(self, a, b):
+        roota = self.find(a)
+        rootb = self.find(b)
+
+        if roota==rootb:
+            return
+        if self.sizelist[roota] < self.sizelist[rootb]:
+            self.sizelist[rootb]+=self.sizelist[roota]
+            self.rootlist[roota] = rootb
+        else:
+            self.sizelist[roota] += self.sizelist[rootb]
+            self.rootlist[rootb] = roota
+        self.count-=1
