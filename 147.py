6802828afb57d15bb21b7d67826cd901f39451b1
zxh <2726012413@qq.com>
2018-11-08 12:49:46 +0800

    147

--- 147.py
+++ 147.py
@@ -12,7 +12,25 @@ class Solution(object):
         """
         if head == None:
             return None
-        cur = head
-        next = head.next
+        node = ListNode(0)
+        node.next = head
+
+        cur = head.next
+        cur_pre = head
         while cur:
-            
+            pre = node
+            next = cur.next
+            flag = True
+            while pre.next != cur:
+                if pre.next.val > cur.val:
+                    flag = False
+                    cur_pre.next = cur.next
+                    cur.next = pre.next
+                    pre.next = cur
+                    break
+                pre = pre.next
+            cur = next
+            if flag==True:
+                cur_pre = cur_pre.next
+        return node.next
+
